d03b2ec5497d26ea988063709b7b598f8640045e
Robert O'Rourke <robert@o-rourke.org>
2024-08-11 13:17:47 +0100

    Add plugin code

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# wp-rsp-img
-Enhances WP images to load the optimal srcset option using the `rsp-img` custom built in element
+# WP Responsive Images
+
+Enhances WP images to load the optimal srcset option using the [`rsp-img` custom built in element](https://github.com/roborourke/rsp-img).
+
+## Installation
+
+Download the zip of this repo, copy to your `wp-content/plugins` directory, then activate the plugin via the admin.
---
+++ wp-rsp-img.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: WP Responsive Images
+Plugin URI: https://github.com/roborourke/wp-rsp-img
+Description: Enhances image tags to use the optimal srcset option.
+Version: 1.0.0
+Author: Robert O'Rourke
+Author URI: https://röb.co
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+// Enqueue the rsp-img.min.js script on the frontend
+function wp_rsp_img_enqueue_scripts() {
+    wp_enqueue_script(
+        'rsp-img',
+        plugins_url( 'rsp-img.min.js', __FILE__ ),
+        '1.0.0',
+        [ 'strategy' => 'async' ]
+    );
+}
+
+add_action( 'wp_enqueue_scripts', 'wp_rsp_img_enqueue_scripts' );
+add_action( 'enqueue_block_editor_assets', 'wp_rsp_img_enqueue_scripts' );
+
+/**
+ * Filter images to use the enhanced responsive mode.
+ *
+ * @param array $attr Key / value pairs of attributes to set on the image tags.
+ * @return array
+ */
+function wp_rsp_img_modify_image_attributes( $attr ) {
+    $attr['is'] = 'rsp-img';
+    return $attr;
+}
+
+add_filter( 'wp_get_attachment_image_attributes', 'wp_rsp_img_modify_image_attributes', 10, 3 );
